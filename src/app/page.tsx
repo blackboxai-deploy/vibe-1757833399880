e1f249420427cdb944cddc929140c1b392964cd0
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-14 12:33:27 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { InventoryDashboard } from '@/components/InventoryDashboard';
+
+export default function HomePage() {
+  return (
+    <main>
+      <InventoryDashboard />
+    </main>
+  );
+}
